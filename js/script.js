3b0decc1933a19d30dbb96b9d2125995f7b168e3
JaZa <47424360+JaZax@users.noreply.github.com>
2019-08-04 22:02:35 +0200

    +ball move without collision

--- js/script.js
+++ js/script.js
@@ -2,6 +2,17 @@ const canvas = document.querySelector('canvas')
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 
+let gamestart = false;
+
+canvas.addEventListener('click', ()=>{
+    if(gamestart == false)
+    {
+        gamestart = true
+    }else{
+        event.preventDefault()
+    }
+})
+
 const c = canvas.getContext('2d')
 
 const controls = {
@@ -40,9 +51,19 @@ window.addEventListener('keydown' , (e)=>{
 let lefty = window.innerHeight/3;
 let righty = window.innerHeight/3;
 
+let ball = {
+
+    "y" : window.innerHeight/2,
+    "x" : window.innerWidth/2,
+    "radius" : 40,
+    "yspeed" : 8,
+    "xspeed" : 8,
+
+}
+
 function action (){
     requestAnimationFrame(action);
-    
+
     c.clearRect(0, 0, window.innerWidth, window.innerHeight)
 
     if(lefty + controls.height > window.innerHeight)
@@ -59,17 +80,45 @@ function action (){
         righty = 12.333333333333314
     }
 
-    c.beginPath();
-    c.arc(window.innerWidth/2, window.innerHeight/2, 40, 0, 2 * Math.PI);
-    c.fillStyle = "#fecb06";
-    c.fill();
-
     c.fillStyle = '#333'
 
     //left control
+
     c.fillRect(100, lefty, controls.width, controls.height)
+
     //right control
+
     c.fillRect(window.innerWidth - 150, righty, 60, window.innerHeight/3)
+
+    //ball 
+
+    c.beginPath();
+    c.arc(ball.x , ball.y, ball.radius, 0, 2 * Math.PI);
+    c.fillStyle = "#fecb06";
+    c.fill();
+
+    if(gamestart == true)
+    {
+
+        if(ball.y + ball.radius > window.innerHeight || ball.y - ball.radius < 0)
+        {
+            
+            ball.yspeed = -ball.yspeed
+
+        }
+
+        if(ball.x + ball.radius > window.innerWidth || ball.x - ball.radius  < 0)
+        {
+            
+            ball.xspeed = -ball.xspeed
+
+        }
+
+        ball.x += ball.xspeed;
+        ball.y += ball.yspeed;
+        
+    }
+
 }
 
 action()
