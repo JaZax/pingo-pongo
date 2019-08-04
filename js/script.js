6af064b638f08a6fadcc3d47a39a67c71e2f053c
JaZa <47424360+JaZax@users.noreply.github.com>
2019-08-04 21:03:28 +0200

    +limited controls move

--- js/script.js
+++ js/script.js
@@ -4,9 +4,17 @@ canvas.width = window.innerWidth;
 
 const c = canvas.getContext('2d')
 
+const controls = {
+
+    "height" : window.innerHeight / 3,
+    "width" : 60,
+
+}
 
 window.addEventListener('keydown' , (e)=>{
-    switch(e.key)
+    if(e.key)
+    {
+        switch(e.key)
     {
         case 'w' : 
             lefty-=25
@@ -24,6 +32,9 @@ window.addEventListener('keydown' , (e)=>{
             righty+=25
             break;
     }
+    }else{
+        console.log('keydown error')
+    }
 })
 
 let lefty = window.innerHeight/3;
@@ -31,12 +42,35 @@ let righty = window.innerHeight/3;
 
 function action (){
     requestAnimationFrame(action);
-
+    
     c.clearRect(0, 0, window.innerWidth, window.innerHeight)
 
-    c.fillStyle = 'rgb(56, 56, 56)'
-    c.fillRect(100, lefty, 60, window.innerHeight/3)
+    if(lefty + controls.height > window.innerHeight)
+    {
+        lefty = 612.3333333333333
+    }else if(lefty < 0){
+        lefty = 12.333333333333314
+    }
+
+    if(righty + controls.height > window.innerHeight)
+    {
+        righty = 612.3333333333333
+    }else if(righty < 0){
+        righty = 12.333333333333314
+    }
+
+    c.beginPath();
+    c.arc(window.innerWidth/2, window.innerHeight/2, 40, 0, 2 * Math.PI);
+    c.fillStyle = "#fecb06";
+    c.fill();
+
+    c.fillStyle = '#333'
+
+    //left control
+    c.fillRect(100, lefty, controls.width, controls.height)
+    //right control
     c.fillRect(window.innerWidth - 150, righty, 60, window.innerHeight/3)
 }
 
 action()
+
